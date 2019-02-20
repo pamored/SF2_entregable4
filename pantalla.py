7ae6a719ab3df5a3fada006199f3a84624578659
Eduardo <edu_7398@hotmail.com>
2019-02-20 18:50:59 -0500

    Cambios de pantallas e implementacion de red en camino

--- pantalla.py
+++ pantalla.py
@@ -1,6 +1,5 @@
-import pygame
-import sys
-import os
+import pygame, random, sys, os
+from red import Red
 
 pygame.font.init()
 
@@ -11,18 +10,38 @@ input_box = pygame.Rect(317,291,164,21)
 
 fondo = ""
 apodo = ""
-personaje = ""
+personaje1 = ""
+personaje2 = ""
 
 velocidad_v = 3
 velocidad_h = 5
 
-class Personaje(pygame.sprite.Sprite):
-    def __init__(self, imagen, num, pos=[10,10]):
+class Jugador(pygame.sprite.Sprite):
+    def __init__(self, imagen,num,enemigo=None):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(os.getcwd() + "/images/personajes/" + imagen + "/" + imagen + " camina"+str(num)+".png")
-        self.image_rect = self.image.get_rect()
-        self.image_rect.topleft = (pos)
-        self.id = imagen
+        self.persona_viva = []
+        for i in range(3):
+            self.persona_viva.append(pygame.image.load(os.getcwd() + "/images/personajes/" + imagen + "/" + imagen + " camina"+(num+1)+".png"))
+        self.image_rect = self.persona_viva[0].get_rect()
+        self.image_rect.topleft = (0,400) if num == "jugador1" else (800-self.image_rect.width,400)
+        self.tomate = Bala("tomate")
+        self.vida = 100
+        self.enemigo = enemigo
+
+    def bajar_vida(self,bala):
+        if bala == "tomate":
+            self.vida -= 5
+        else:
+            self.vida -= 15
+
+class Bala(pygame.sprite.Sprite):
+    def __init__(self,bala):
+        self.bala = pygame.image.load(os.getcwd()+"images/disparos/"+bala+".png")
+        self.rect_bala = self.bala.get_rect()
+        self.visible = False
+
+    def disparo(self):
+        pass
 
 class Fondo(pygame.sprite.Sprite):
     def __init__(self, fondo, pos=[0,0], size=(800,600)):
@@ -34,16 +53,6 @@ class Fondo(pygame.sprite.Sprite):
         self.ancho = size[0]
         self.alto = size[1]
         self.id = fondo
-        
-class Boton(pygame.sprite.Sprite):
-    def __init__(self, boton, pos, size=(130,30)):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(os.getcwd() + "/images/botones/" + boton + ".png")
-        self.image = pygame.transform.scale(self.image, size)
-        self.x = pos[0]
-        self.y = pos[1]
-        self.ancho = size[0]
-        self.alto = size[1]
 
 class Pantalla:
     def __init__(self,manager):
@@ -72,78 +81,63 @@ class Pantalla:
 class ListaEscenarios(Pantalla):
     def __init__(self,manager):
         Pantalla.__init__(self,manager)
-        #Imagenes a usar
-        nom_fondos = ["fondo","palacio","aeropuerto","plaza(dia)","plaza(noche)"]
-        global fondo
-        fondo = "fondo"
-        fuente = pygame.font.SysFont("Arial",22)
-        self.textos = []
-        self.textos.append(fuente.render("Palacio de Gobierno",0,(166,166,192),(27,27,113)))
-        self.textos.append(fuente.render("Aeropuerto Jorge Chavez",0,(166,166,192),(27,27,113)))
-        self.textos.append(fuente.render("Plaza de Armas (Dia)",0,(166,166,192),(27,27,113)))
-        self.textos.append(fuente.render("Plaza de Armas(Noche)",0,(166,166,192),(27,27,113)))
-        for nom in nom_fondos:
-            self.agregar_sprite(nom,Fondo(nom))
-
+        self.lista_escenarios = ["","plaza","ulima","","nazca","machu","","aeropuerto","plaza"]
+        self.ubicaciones = [(17,73),(202,74),(391,77),(581,77),(16,230),(200,231),(389,234),(582,235),(14,402)]
+        self.id = None
+        self.agregar_sprite("fondo",Fondo("escenarios"))
         self.set_nombre_ventana("Escoger Escenario")
+        #self.rect_pos = (0,0)
+        #self.rect = pygame.draw.rect(self.canvas,(255,127,80),(self.rect_pos,))
         self.handle_events()
         self.update()
 
     def handle_events(self):
         global fondo
-        x,y = pygame.mouse.get_pos()
-        if x >=300 and x <= 448 and y >= 100 and y <= 122:
-            fondo = self.sprites["palacio"].id
-        elif x >=300 and x <= 490 and y >= 200 and y <= 222:
-            fondo = self.sprites["aeropuerto"].id
-        elif x >=300 and x <= 450 and y >= 300 and y <= 322:
-            fondo = self.sprites["plaza(dia)"].id
-        elif x >=300 and x <= 472 and y >= 400 and y <= 422:
-            fondo = self.sprites["plaza(noche)"].id
-        else:
-            fondo = "fondo"
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit(); sys.exit()
-            elif event.type == pygame.MOUSEBUTTONUP:
-                if x >=300 and x <= 420 and y >= 100 and y <= 122:
-                    self.manager.cambiar_pantalla(ListaPersonajes(self.manager))
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                print(pygame.mouse.get_pos())
+                """if x >=17 and x <= 178 and y >= 73 and y <= 196:
+                    self.id = 0
+                elif x >=300 and x <= 500 and y >= 74 and y <= 222:
+                    self.id = 0
+                elif x >=300 and x <= 400 and y >= 77 and y <= 322:
+                    self.id = 0
+                elif x >=300 and x <= 480 and y >= 77 and y <= 422:
+                    self.id = 0
+                elif x >=300 and x <= 500 and y >= 230 and y <= 222:
+                    self.id = 0
+                elif x >=300 and x <= 500 and y >= 231 and y <= 222:
+                    self.id = 0
+                elif x >=300 and x <= 500 and y >= 234 and y <= 222:
+                    self.id = 0
+                elif x >=300 and x <= 500 and y >= 235 and y <= 222:
+                    self.id = 0
+                elif x >=300 and x <= 500 and y >= 402 and y <= 222:
+                    self.id = 0
                 elif x >=300 and x <= 500 and y >= 200 and y <= 222:
-                    self.manager.cambiar_pantalla(ListaPersonajes(self.manager))
-                elif x >=300 and x <= 400 and y >= 300 and y <= 322:
-                    self.manager.cambiar_pantalla(ListaPersonajes(self.manager))
-                elif x >=300 and x <= 480 and y >= 400 and y <= 422:
-                    self.manager.cambiar_pantalla(ListaPersonajes(self.manager))
+                    self.id = random.randint(0,9)
+                elif x >=300 and x <= 500 and y >= 200 and y <= 222:
+                    if self.id != None:
+                        fondo = self.lista_escenarios[self.id]
+                        """
 
     def render(self):
-        global fondo
-        surf = pygame.Surface((self.sprites[fondo].ancho, self.sprites[fondo].alto))
-        self.canvas.blit(self.sprites[fondo].image, surf.get_rect(topleft=(self.sprites[fondo].x,
-        self.sprites[fondo].y)))
-        for i in range(len(self.textos)):
-            self.canvas.blit(self.textos[i],(300,(i+1)*100))
+        surf = pygame.Surface((self.sprites["fondo"].ancho, self.sprites["fondo"].alto))
+        self.canvas.blit(self.sprites["fondo"].image, surf.get_rect(topleft=(self.sprites["fondo"].x,
+        self.sprites["fondo"].y)))
+
         pygame.display.flip()    
 
 class ListaPersonajes(Pantalla):
     def __init__(self,manager):
         Pantalla.__init__(self,manager)
-        self.agregar_sprite("fondo",Fondo("fondo"))
+        self.agregar_sprite("fondo",Fondo("jugadores"))
         #Imagenes a usar
-        nom_personajes = ["alan","belaunde","fujimori","grau","jose","ollanta","pierola","ppk","riva","toledo"]
-        fuente = pygame.font.SysFont("Arial",22)
-        self.textos = []
-        self.textos.append(fuente.render("Alan Garcia",0,(166,166,192),(27,27,113)))
-        self.textos.append(fuente.render("Fernando Belaunde Terry",0,(166,166,192),(27,27,113)))
-        self.textos.append(fuente.render("Alberto Fujimori",0,(166,166,192),(27,27,113)))
-        self.textos.append(fuente.render("Miguel Grau",0,(166,166,192),(27,27,113)))
-        self.textos.append(fuente.render("José Domingo Pérez",0,(166,166,192),(27,27,113)))
-        self.textos.append(fuente.render("Ollanta Humala",0,(166,166,192),(27,27,113)))
-        self.textos.append(fuente.render("Nicolás de Pierola",0,(166,166,192),(27,27,113)))
-        self.textos.append(fuente.render("Pedro Pablo Kuczynski",0,(166,166,192),(27,27,113)))
-        self.textos.append(fuente.render("José de la Riva Agüero",0,(166,166,192),(27,27,113)))
-        self.textos.append(fuente.render("Alejandro Toledo",0,(166,166,192),(27,27,113)))
-        for nom in nom_personajes:
-            self.agregar_sprite(nom,Personaje(nom,1,[450,250]))
+        self.nom_personajes = ["alan","belaunde","fujimori","grau","jose","ollanta","pierola","ppk","riva","toledo"]
+        self.ubicaciones = [(56,134),(144,134),(232,134),(320,134),(408,134),(0,342),(85,342),(175,342),(263,342),(352,342)]
+        self.id = None
 
         self.set_nombre_ventana("Escoger Personaje")
         self.handle_events()
@@ -151,83 +145,80 @@ class ListaPersonajes(Pantalla):
 
     def handle_events(self):
         global personaje
-        x,y = pygame.mouse.get_pos()
-        if x >=200 and x <= 300 and y >= 50 and y <= 72:
-            personaje = self.sprites["alan"].id
-        elif x >=200 and x <= 300 and y >= 100 and y <= 122:
-            personaje = self.sprites["belaunde"].id
-        elif x >=200 and x <= 300 and y >= 150 and y <= 172:
-            personaje = self.sprites["fujimori"].id
-        elif x >=200 and x <= 300 and y >= 200 and y <= 222:
-            personaje = self.sprites["grau"].id
-        elif x >=200 and x <= 300 and y >= 250 and y <= 272:
-            personaje = self.sprites["jose"].id
-        elif x >=200 and x <= 300 and y >= 300 and y <= 322:
-            personaje = self.sprites["ollanta"].id
-        elif x >=200 and x <= 300 and y >= 350 and y <= 372:
-            personaje = self.sprites["pierola"].id
-        elif x >=200 and x <= 300 and y >= 400 and y <= 422:
-            personaje = self.sprites["ppk"].id
-        elif x >=200 and x <= 300 and y >= 450 and y <= 472:
-            personaje = self.sprites["riva"].id
-        elif x >=200 and x <= 300 and y >= 500 and y <= 522:
-            personaje = self.sprites["toledo"].id
-        else:
-            personaje = ""
+        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit(); sys.exit()
-            elif event.type == pygame.MOUSEBUTTONUP:
-                if x >=200 and x <= 300 and y >= 50 and y <= 72:
-                    self.manager.cambiar_pantalla(Juego(self.manager))
-                elif x >=200 and x <= 300 and y >= 100 and y <= 122:
-                    self.manager.cambiar_pantalla(Juego(self.manager))
-                elif x >=200 and x <= 300 and y >= 150 and y <= 172:
-                    self.manager.cambiar_pantalla(Juego(self.manager))
-                elif x >=200 and x <= 300 and y >= 200 and y <= 222:
-                    self.manager.cambiar_pantalla(Juego(self.manager))
-                elif x >=200 and x <= 300 and y >= 250 and y <= 272:
-                    self.manager.cambiar_pantalla(Juego(self.manager))
-                elif x >=200 and x <= 300 and y >= 300 and y <= 322:
-                    self.manager.cambiar_pantalla(Juego(self.manager))
-                elif x >=200 and x <= 300 and y >= 350 and y <= 372:
-                    self.manager.cambiar_pantalla(Juego(self.manager))
-                elif x >=200 and x <= 300 and y >= 400 and y <= 422:
-                    self.manager.cambiar_pantalla(Juego(self.manager))
-                elif x >=200 and x <= 300 and y >= 450 and y <= 472:
-                    self.manager.cambiar_pantalla(Juego(self.manager))
-                elif x >=200 and x <= 300 and y >= 500 and y <= 522:
-                    self.manager.cambiar_pantalla(Juego(self.manager))
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                x,y = pygame.mouse.get_pos()
+                print (x)
+                if x >=56 and x <= 134 and y >= 135 and y <= 285:
+                    self.id = 0
+                elif x >=144 and x <= 222 and y >= 135 and y <= 285:
+                    self.id = 1
+                elif x >=232 and x <= 310 and y >= 135 and y <= 285:
+                    self.id = 2
+                elif x >=320 and x <= 398 and y >= 135 and y <= 285:
+                    self.id = 3
+                elif x >=408 and x <= 486 and y >= 135 and y <= 285:
+                    self.id = 4
+                elif x >=0 and x <= 77 and y >= 342 and y <= 492:
+                    self.id = 5
+                elif x >=85 and x <= 165 and y >= 342 and y <= 492:
+                    self.id = 6
+                elif x >=175 and x <= 255 and y >= 342 and y <= 492:
+                    self.id = 7
+                elif x >=263 and x <= 342 and y >= 342 and y <= 492:
+                    self.id = 8
+                elif x >=352 and x <= 432 and y >= 342 and y <= 492:
+                    self.id = 9
+                elif x >=208 and x <= 375 and y >= 544 and y <= 600:
+                    self.id = random.randint(0,9)
+                elif x >=30 and x <= 199 and y >= 544 and y <= 600:
+                    if self.id != None:
+                        personaje = self.nom_personajes[self.id]
+                        self.manager.cambiar_pantalla(Juego(self.manager))
+                else:
+                    self.id = None
 
     def render(self):
         surf = pygame.Surface((self.sprites["fondo"].ancho, self.sprites["fondo"].alto))
         self.canvas.blit(self.sprites["fondo"].image, surf.get_rect(topleft=(self.sprites["fondo"].x,
         self.sprites["fondo"].y)))
-        if personaje != "":
-            self.canvas.blit(self.sprites[personaje].image, self.sprites[personaje].image_rect)
-        for i in range(len(self.textos)):
-            self.canvas.blit(self.textos[i],(200,(i+1)*50))
+        if self.id != None:
+            pygame.draw.rect(self.canvas,(255,127,80),(self.ubicaciones[self.id][0],self.ubicaciones[self.id][1],80,150),2)
         pygame.display.flip()
 
 class Juego(Pantalla):
     def __init__(self,manager):
-        global personaje, fondo
+        self.net = Red()
+        global personaje1, personaje2, fondo
         Pantalla.__init__(self,manager)
         self.agregar_sprite("fondo",Fondo(fondo))
-        sprites_persona = []
+        jugador = "jugador1" if self.net.id == "0" else "jugador2"
+        jugador2 = "jugador2" if jugador == "jugador1" else "jugador1"
+        personaje2 = self.parse_data(self.net.send(str(self.net.id) + ":" + personaje1))
+        self.jugador1 = Jugador(personaje1,jugador)
+        self.jugador2 = Jugador(personaje2,jugador2)
+        self.jugador1.enemigo = self.jugador2
+        self.jugador2.enemigo = self.jugador1
 
-        for i in range(3):
-            sprites_persona.append(Personaje(personaje,(i+1)))
-
-        self.agregar_sprite(personaje,sprites_persona)
         self.id = 0
-        self.imagen = self.sprites[personaje][0].image
-        self.rect_persona = self.sprites[personaje][0].image_rect
-        self.rect_persona.topleft = (0,400)
-        self.izquierda = False
-        self.set_nombre_ventana("Prueba de jugabilidad")
+        self.imagen = self.jugador1.persona_viva[0]
+        self.rect_persona = self.jugador1.image_rect
+        self.izquierda = False if self.net.id == "0" else True
+        self.set_nombre_ventana("Jugando")
         self.handle_events()
         self.update()
+
+    @staticmethod
+    #Los une y lo hace por cada host
+    def parse_data(data):
+        try:
+            d = data.split(":")[1]
+            return d
+        except:
+            return ""
 
     def handle_events(self):
         global personaje
@@ -237,31 +228,31 @@ class Juego(Pantalla):
             if self.id > 2:
                 self.id = 0
             if self.izquierda:
-                self.imagen = pygame.transform.flip(self.sprites[personaje][self.id].image,True,False)
+                self.imagen = pygame.transform.flip(self.jugador1.persona_viva[self.id],True,False)
             else:
-                self.imagen = self.sprites[personaje][self.id].image
+                self.imagen = self.jugador1.persona_viva[self.id]
             self.rect_persona.y -= velocidad_v
         if keys[pygame.K_s] and self.rect_persona.y < (600-self.rect_persona.height):
             self.id += 1
             if self.id > 2:
                 self.id = 0
             if self.izquierda:
-                self.imagen = pygame.transform.flip(self.sprites[personaje][self.id].image,True,False)
+                self.imagen = pygame.transform.flip(self.jugador1.persona_viva[self.id],True,False)
             else:
-                self.imagen = self.sprites[personaje][self.id].image
+                self.imagen = self.jugador1.persona_viva[self.id]
             self.rect_persona.y += velocidad_v
         if keys[pygame.K_a] and self.rect_persona.x > 0:
             self.id += 1
             if self.id > 2:
                 self.id = 0
-            self.imagen = pygame.transform.flip(self.sprites[personaje][self.id].image,True,False)
+            self.imagen = pygame.transform.flip(self.jugador1.persona_viva[self.id],True,False)
             self.izquierda = True
             self.rect_persona.x -= velocidad_h
         if keys[pygame.K_d] and self.rect_persona.x < (800-self.rect_persona.width):
             self.id += 1
             if self.id > 2:
                 self.id = 0
-            self.imagen = self.sprites[personaje][self.id].image
+            self.imagen = self.jugador1.persona_viva[self.id]
             self.izquierda = False
             self.rect_persona.x += velocidad_h
 
@@ -280,20 +271,11 @@ class Login(Pantalla):
         Pantalla.__init__(self,manager)
         self.text = ""
         self.active = False
-        coordenadas = {
-        "salir": (74,508),
-        "jugar": (335,320),
-        "registrar": (597,405),
-        "creditos": (597,511)}
 
         self.set_nombre_ventana("Login")
 
         #este es el fondo
-        self.sprites["background"] = Fondo("login1")
-
-        #botones del login
-        for nom,cor in coordenadas.items():
-            self.sprites[nom] = Boton(nom,cor)
+        self.sprites["background"] = Fondo("login")
         
     
     def handle_events(self):
@@ -302,15 +284,12 @@ class Login(Pantalla):
             if event.type == pygame.QUIT:
                 pygame.quit(); sys.exit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                salir_rect = self.sprites["salir"].image.get_rect()
-                salir_rect.topleft = (self.sprites["salir"].x,self.sprites["salir"].y)
-                jugar_rect = self.sprites["jugar"].image.get_rect()
-                jugar_rect.topleft = (self.sprites["jugar"].x,self.sprites["jugar"].y)
+                x,y = pygame.mouse.get_pos()
                 if input_box.collidepoint(event.pos):
                     self.active = True
-                elif salir_rect.collidepoint(event.pos):
+                elif x>=74 and x<=204 and y>=508 and y<=537:
                     pygame.quit(); sys.exit()
-                elif jugar_rect.collidepoint(event.pos):
+                elif x>=335 and x<=465 and y>=320 and y<=350:
                     if self.text != "":
                         apodo = self.text
                         self.manager.cambiar_pantalla(ListaEscenarios(self.manager))
